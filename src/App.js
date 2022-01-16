f4bc5237e20ba25193d58967e8fc867189311756
Muhammad Rafli <rafliogun49@gmail.com>
2022-01-16 11:16:03 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from '../src/components/Navbar'
+import Hero from '../src/components/Hero'
+import Card from '../src/components/Card'
+import data from "./data"
 
-function App() {
+
+export default function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card
+      key={item.id} 
+      {...item}
+      />
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+      {cards}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src='./images/logo-airbnb.png' alt="Airbnb" className="nav--logo" />
+        </nav>
+    )
+}
